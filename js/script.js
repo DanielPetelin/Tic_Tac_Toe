a0f5022a7ec42d8b14cdb924288ebf0e36af2a21
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-11-30 16:54:33 +0300

    X

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.getElementById('game').onclick = function(evnt) {
     console.log(event);
     if (event.target.className == 'block') {
         if (hod % 2 == 0) {
-           event.target.innerHTML = 'x'; 
+           event.target.innerHTML = 'X'; 
         } else {
             event.target.innerHTML = '0'; 
         }
