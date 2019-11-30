421792be17023f3ce98bdc3eeec685705361c954
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-11-30 16:49:27 +0300

    hod

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ for (var i = 0; i < 9; i++) {
     document.getElementById('game').innerHTML+='<div class="block"></div>';
 }
 
+var hod = 0;
+
 document.getElementById('game').onclick = function(evnt) {
     console.log(event);
     if (event.target.className == 'block') {
-        this.innerHTML = '0'
+        event.target.innerHTML = '0';
     }
 }
